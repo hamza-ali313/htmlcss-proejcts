051c177f264bd84f0a5c1ede6630cb5d884a6679
hamza ali <70458340+hamza-ali313@users.noreply.github.com>
2022-11-24 17:08:59 -0800

    push

---
+++ htmlcss-proejcts/DubaiVisa PSD TO html/js/custom.js
@@ -0,0 +1,192 @@
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 4,
+  responsive: [
+    {
+      breakpoint: 1024,
+      settings: {
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        infinite: true,
+        dots: true
+      }
+    },
+    {
+      breakpoint: 600,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
+
+
+// $('.simple-btns button').on('click', function(){
+//   $('.simple-btns button.current').removeClass('current');
+//   $(this).addClass('current');
+// });
+$('.simple-btns button').on('click',function(){
+  $('.simple-btns button.current').removeClass('current');
+  $(this).addClass('current');
+})
+
+$('.radio-btns Label').on('click',function(){
+  $('.radio-btns Label.currentLabel').removeClass('currentLabel');
+  $(this).addClass('currentLabel')
+})
+
+
+// Gallery
+// 
+
+$(document).ready(function(){
+
+    $(".filter-button").click(function(){
+        var value = $(this).attr('data-filter');
+        
+        if(value == "all")
+        {
+            //$('.filter').removeClass('hidden');
+            $('.filter').show('1000');
+        }
+        else
+        {
+//            $('.filter[filter-item="'+value+'"]').removeClass('hidden');
+//            $(".filter").not('.filter[filter-item="'+value+'"]').addClass('hidden');
+            $(".filter").not('.'+value).hide('3000');
+            $('.filter').filter('.'+value).show('3000');
+            
+        }
+    });
+    
+    if ($(".filter-button").removeClass("active")) {
+$(this).removeClass("active");
+}
+$(this).addClass("active");
+
+});
+
+
+
+
+
+
+
+
+// Menu Js
+(function($) {
+$.fn.menumaker = function(options) {  
+ var cssmenu = $(this), settings = $.extend({
+   format: "dropdown",
+   sticky: false
+ }, options);
+ return this.each(function() {
+   $(this).find(".button").on('click', function(){
+     $(this).toggleClass('menu-opened');
+     var mainmenu = $(this).next('ul');
+     if (mainmenu.hasClass('open')) { 
+       mainmenu.slideToggle().removeClass('open');
+     }
+     else {
+       mainmenu.slideToggle().addClass('open');
+       if (settings.format === "dropdown") {
+         mainmenu.find('ul').show();
+       }
+     }
+   });
+   cssmenu.find('li ul').parent().addClass('has-sub');
+multiTg = function() {
+     cssmenu.find(".has-sub").prepend('<span class="submenu-button"></span>');
+     cssmenu.find('.submenu-button').on('click', function() {
+       $(this).toggleClass('submenu-opened');
+       if ($(this).siblings('ul').hasClass('open')) {
+         $(this).siblings('ul').removeClass('open').slideToggle();
+       }
+       else {
+         $(this).siblings('ul').addClass('open').slideToggle();
+       }
+     });
+   };
+   if (settings.format === 'multitoggle') multiTg();
+   else cssmenu.addClass('dropdown');
+   if (settings.sticky === true) cssmenu.css('position', 'fixed');
+resizeFix = function() {
+  var mediasize = 1000;
+     if ($( window ).width() > mediasize) {
+       cssmenu.find('ul').show();
+     }
+     if ($(window).width() <= mediasize) {
+       cssmenu.find('ul').hide().removeClass('open');
+     }
+   };
+   resizeFix();
+   return $(window).on('resize', resizeFix);
+ });
+  };
+})(jQuery);
+
+(function($){
+$(document).ready(function(){
+$("#cssmenu").menumaker({
+   format: "multitoggle"
+});
+});
+})(jQuery);
+
+
+
+
+
+// country drop down
+function setCountry(code){
+  if(code || code==''){
+      var text = jQuery('a[cunt_code="'+code+'"]').html();
+      $(".dropdown dt a span").html(text);
+  }
+}
+$(document).ready(function() {
+  $(".dropdown img.flag").addClass("flagvisibility");
+
+  $(".dropdown dt a").click(function() {
+      $(".dropdown dd ul").toggle();
+  });
+
+  $(".dropdown dd ul li a").click(function() {
+      //console.log($(this).html())
+      var text = $(this).html();
+      $(".dropdown dt a span").html(text);
+      $(".dropdown dd ul").hide();
+      $("#result").html("Selected value is: " + getSelectedValue("country-select"));
+  });
+
+  function getSelectedValue(id) {
+      //console.log(id,$("#" + id).find("dt a span.value").html())
+      return $("#" + id).find("dt a span.value").html();
+  }
+
+  $(document).bind('click', function(e) {
+      var $clicked = $(e.target);
+      if (! $clicked.parents().hasClass("dropdown"))
+          $(".dropdown dd ul").hide();
+  });
+
+
+  $("#flagSwitcher").click(function() {
+      $(".dropdown img.flag").toggleClass("flagvisibility");
+  });
+});
+// country drop down
